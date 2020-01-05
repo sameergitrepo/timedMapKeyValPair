046a54aeb95ae4af69bb3a26321ed030d37b97a7
Sameer <sameer.a@jinigram.com>
2020-01-05 23:57:43 +0530

    added map

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	timemap := make(map[string]*time.Timer)
+	fmt.Print("\n1. adding a key ")
+	timemap["name1"] = time.NewTimer(time.Second * 10)
+	fmt.Print("\n 2. adding second key..")
+	timemap["name2"] = time.NewTimer(time.Second * 10)
+	fmt.Print("\n adding third key..")
+	timemap["name3"] = time.NewTimer(time.Second * 10)
+
+	fmt.Print("\n length of map is ", len(timemap))
+
+	time.Sleep(time.Second * 5)
+	fmt.Print("\n length of map is ", len(timemap))
+	time.Sleep(time.Second * 5)
+	fmt.Print("\n length of map is ", len(timemap))
 }
