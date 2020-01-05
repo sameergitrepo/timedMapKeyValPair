dd9c3ac5a3f13519a963727652f9329309f2a1a0
Sameer <sameer.a@jinigram.com>
2020-01-06 01:34:46 +0530

    final commit

--- main.go
+++ main.go
@@ -11,24 +11,26 @@ func main() {
 	timemap := make(TimeMap, 0)
 
 	fmt.Print("\n1. adding a key ")
-	timemap["name1"] = time.NewTimer(time.Second * 10)
-	go timemap.Delete(timemap["name1"].C, "name1")
-	fmt.Print("\n 2. adding second key..")
-	timemap["name2"] = time.NewTimer(time.Second * 10)
-	fmt.Print("\n adding third key..")
-	timemap["name3"] = time.NewTimer(time.Second * 10)
+
+	timemap.AddToMap("name1", 10)
+	timemap.AddToMap("name2", 15)
+	timemap.AddToMap("name3", 20)
 
 	fmt.Print("\n length of map is ", len(timemap))
-
+	fmt.Print("\nsleeping...")
 	time.Sleep(time.Second * 10)
+	fmt.Print("\n length of map after sleeping for 10 seconds is ..")
 	fmt.Print("\n length of map is ", len(timemap))
+	fmt.Print("\n length of map after sleeping for 5 seconds is ..")
 	time.Sleep(time.Second * 5)
-	fmt.Print("\n length of map is ", len(timemap))
+	fmt.Print("\n final length of map is ", len(timemap))
 }
 
 func (m TimeMap) Delete(timech <-chan time.Time, key string) {
+	fmt.Print("\n blocked for value ", key)
 	<-timech
 	delete(m, key)
+	fmt.Print("\n deleted key.. ", key)
 }
 
 func (m TimeMap) AddToMap(key string, timervalinsec int) {
